4f4de370e82e66b08e8c5f0582994eacbc67af9a
Joe Harris <josephharris@opensignal.com>
2017-09-28 10:58:17 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name = 'pymmh3',
+    version = '1.0',
+    description = 'Pure python implementation ',
+    license = 'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+    py_modules = ['pymmh3'],
+    keywords = "utility hash MurmurHash",
+    long_description = open('README.md').read(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities'
+    ]
+)
